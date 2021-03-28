3312a1f1c0c39d1ca12bb87e62b55023ae9244a1
Benny Franco <bennyfranco@icloud.com>
2021-03-28 00:13:24 -0700

    Draw circles

--- UnityComputeShaders - start/Assets/Scripts/PassData.cs
+++ UnityComputeShaders - start/Assets/Scripts/PassData.cs
@@ -11,6 +11,7 @@ public class PassData : MonoBehaviour
     RenderTexture outputTexture;
 
     int circlesHandle;
+    int clearHandler;
 
     public Color clearColor = new Color();
     public Color circleColor = new Color();
@@ -31,21 +32,28 @@ public class PassData : MonoBehaviour
     private void InitShader()
     {
         circlesHandle = shader.FindKernel("Circles");
+        clearHandler = shader.FindKernel("Clear");
 
-        shader.SetInt( "texResolution", texResolution);
-        shader.SetTexture( circlesHandle, "Result", outputTexture);
+        shader.SetInt("texResolution", texResolution);
+        shader.SetVector("circleColor", circleColor);
+        shader.SetTexture(circlesHandle, "Result", outputTexture);
+
+        shader.SetVector("clearColor", clearColor);
+        shader.SetTexture(clearHandler, "Result", outputTexture);
 
         rend.material.SetTexture("_MainTex", outputTexture);
     }
- 
+
     private void DispatchKernel(int count)
     {
+        shader.Dispatch(clearHandler, texResolution / 8, texResolution / 8, 1);
+        shader.SetFloat("time", Time.time);
         shader.Dispatch(circlesHandle, count, 1, 1);
     }
 
     void Update()
     {
-        DispatchKernel(1);
+        DispatchKernel(10);
     }
 }
 

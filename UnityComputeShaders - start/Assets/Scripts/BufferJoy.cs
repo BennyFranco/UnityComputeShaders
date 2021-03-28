49870429e4d79499c712a2ae0608530ade43d3ed
Benny Franco <bennyfranco@icloud.com>
2021-03-28 00:47:17 -0700

    Compute shaders buffers

--- UnityComputeShaders - start/Assets/Scripts/BufferJoy.cs
+++ UnityComputeShaders - start/Assets/Scripts/BufferJoy.cs
@@ -18,6 +18,16 @@ public class BufferJoy : MonoBehaviour
 
     int count = 10;
 
+    struct Circle
+    {
+        public Vector2 origin;
+        public Vector2 velocity;
+        public float radius;
+    }
+
+    Circle[] circleData;
+    ComputeBuffer buffer;
+
     // Use this for initialization
     void Start()
     {
@@ -36,25 +46,53 @@ public class BufferJoy : MonoBehaviour
     private void InitData()
     {
         circlesHandle = shader.FindKernel("Circles");
+
+        uint threadGroupSizeX;
+        shader.GetKernelThreadGroupSizes(circlesHandle, out threadGroupSizeX, out _, out _);
+        
+        int total = (int)threadGroupSizeX * count;
+        circleData = new Circle[total];
+
+        float speed = 100;
+        float halfSpeed = speed * 0.5f;
+        float minRadius  = 10.0f;
+        float maxRadius = 30.0f;
+        float radiusRange = maxRadius - minRadius;
+
+        for(int i = 0; i < total; i++)
+        {
+            Circle circle = circleData[i];
+            circle.origin.x = Random.value * texResolution;
+            circle.origin.y = Random.value * texResolution;
+            circle.velocity.x = (Random.value * speed) - halfSpeed;
+            circle.velocity.y = (Random.value * speed) - halfSpeed;
+            circle.radius = Random.value *  radiusRange + minRadius;
+            circleData[i] = circle;
+        }
     }
 
     private void InitShader()
     {
-    	clearHandle = shader.FindKernel("Clear");
-    	
-        shader.SetVector( "clearColor", clearColor );
-        shader.SetVector( "circleColor", circleColor );
-        shader.SetInt( "texResolution", texResolution );
-		
-		shader.SetTexture( clearHandle, "Result", outputTexture );
-        shader.SetTexture( circlesHandle, "Result", outputTexture );
+        clearHandle = shader.FindKernel("Clear");
+
+        shader.SetVector("clearColor", clearColor);
+        shader.SetVector("circleColor", circleColor);
+        shader.SetInt("texResolution", texResolution);
+
+        shader.SetTexture(clearHandle, "Result", outputTexture);
+        shader.SetTexture(circlesHandle, "Result", outputTexture);
+
+        int stride = (2 + 2 + 1) * sizeof(float);
+        buffer = new ComputeBuffer(circleData.Length, stride);
+        buffer.SetData(circleData);
+        shader.SetBuffer(circlesHandle, "circlesBuffer", buffer);
 
         rend.material.SetTexture("_MainTex", outputTexture);
     }
- 
+
     private void DispatchKernels(int count)
     {
-    	shader.Dispatch(clearHandle, texResolution/8, texResolution/8, 1);
+        shader.Dispatch(clearHandle, texResolution / 8, texResolution / 8, 1);
         shader.SetFloat("time", Time.time);
         shader.Dispatch(circlesHandle, count, 1, 1);
     }
